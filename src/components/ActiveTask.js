cc0ccec0b46add3a72fa064b1b9001095be62be4
Aditya <kohsa9211420@gmail.com>
2024-02-05 12:43:09 +0530

    chnges

--- src/components/ActiveTask.js
+++ src/components/ActiveTask.js
@@ -18,7 +18,7 @@ const id = props.item.id;
 
   return (
     <div key={props.item.date} className="m-12 pt-4 pl-4 pr-4 pb-4 border-2 border-gray-600 rounded-lg">
-      <div className={`flex items-center ${props.item.category === 'general' ? 'bg-#3490dc' : props.item.category === "urgent" ? 'bg-#ff0000' : props.item.category === "work" ? "bg-#ffb400" : props.item.category === "home" ? "bg-#00cc66" : props.item.category === "learning" ? "bg-#9966cc" : "bg-#ff9900" } p-2 rounded-lg mb-4`}>
+      <div className={`flex items-center ${props.item.category === 'general' ? 'bg-blue-400' : props.item.category === "urgent" ? 'bg-red-600' : props.item.category === "work" ? "bg-yellow-400" : props.item.category === "home" ? "bg-green-600" : props.item.category === "learning" ? "bg-purple-600" : "bg-orange-500" } p-2 rounded-lg mb-4`}>
         <MdDone className="text-white mr-2 cursor-pointer" onClick={()=> handleRemoveTodo()} />
         <h1 className="text-white">{props.item.category}</h1>
       </div>
--- src/components/CompletedTodo.js
+++ src/components/CompletedTodo.js
@@ -11,7 +11,7 @@ const CompletedTodo = (props) => {
     const dateString = props.item.date.toString() ;
   return (
     <div key={props.item.date} className="m-12 pt-4 pl-4 pr-4 pb-4 border-2 border-gray-600 rounded-lg">
-      <div className={`flex items-center ${props.item.category === 'general' ? 'bg-#3490dc' : props.item.category === "urgent" ? 'bg-#ff0000' : props.item.category === "work" ? "bg-#ffb400" : props.item.category === "home" ? "bg-#00cc66" : props.item.category === "learning" ? "bg-#9966cc" : "bg-#ff9900" } p-2 rounded-lg mb-4`}>
+      <div className={`flex items-center ${props.item.category === 'general' ? 'bg-blue-400' : props.item.category === "urgent" ? 'bg-red-600' : props.item.category === "work" ? "bg-yellow-400" : props.item.category === "home" ? "bg-green-600" : props.item.category === "learning" ? "bg-purple-600" : "bg-orange-500" } p-2 rounded-lg mb-4`}>
         
         <h1 className="text-white">{props.item.category}</h1>
       </div>
