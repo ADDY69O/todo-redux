033bb539a54f24873f94a75beacfbdf2faf3c963
Aditya <kohsa9211420@gmail.com>
2024-02-05 12:10:03 +0530

    fix color

--- src/components/CompletedTodo.js
+++ src/components/CompletedTodo.js
@@ -11,7 +11,7 @@ const CompletedTodo = (props) => {
     const dateString = props.item.date.toString() ;
   return (
     <div key={props.item.date} className="m-12 pt-4 pl-4 pr-4 pb-4 border-2 border-gray-600 rounded-lg">
-      <div className={`flex items-center bg-${props.item.category} p-2 rounded-lg mb-4`}>
+      <div className={`flex items-center ${props.item.category === 'general' ? 'bg-general' : props.item.category === "urgent" ? 'bg-urgent' : props.item.category === "work" ? "bg-work" : props.item.category === "home" ? "bg-home" : props.item.category === "learning" ? "bg-learning" : "bg-travel" } p-2 rounded-lg mb-4`}>
         
         <h1 className="text-white">{props.item.category}</h1>
       </div>
